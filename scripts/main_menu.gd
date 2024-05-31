dfdbe56723d9fefa642643b1c3d8f4c9c114c8c9
shadow <144371189+SHADOW-077@users.noreply.github.com>
2024-05-31 22:52:32 +0530

    game

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scene/game.tscn") # loads the game scene when start  button is pressed
+	get_tree().change_scene_to_file("res://scene/grassland.tscn") # loads the game scene when start  button is pressed
 
 
 
